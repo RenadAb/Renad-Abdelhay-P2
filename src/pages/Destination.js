374c1abdbe60a3fc05a3ae46e961e422c1d442d8
Renad Abdelhay <abdelhar@gmatc.matc.edu>
2024-05-01 19:58:49 -0500

    update

---
+++ src/pages/Destination.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Destination = () => (
+  <Layout>
+    <Seo title="Page two"/>
+    <h1>Destinations</h1>
+  </Layout>
+)
+
+
+export default Destinations
